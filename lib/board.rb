a4f7960813f5a9faa96f1202d896157d0f92e976
Apprentice <apprentice@devbootcamp.com>
2012-11-01 12:16:36 -0700

    Board and board spec

---
+++ lib/board.rb
@@ -0,0 +1,92 @@
+class Board
+	def initialize
+		@board = Array.new(7) { Array.new(6) }
+		@last_drop_column = nil
+		@last_drop_row = nil
+		@last_drop_color = nil
+	end
+
+	def drop_disc!(column, color)
+		@board[column-1].each_with_index do |cell, index|
+			if cell.nil?
+				@board[column-1][index] = color
+				@last_drop_column = column - 1
+				@last_drop_row = index
+				@last_drop_color = color
+				return true 
+			end
+		end
+		false
+	end
+
+	def full?
+		@board.each do |column|
+			column.each do |cell|
+				return false if cell.nil?
+			end
+		end
+		true
+	end
+
+	def color_of_connect_four 
+		if @last_drop_column.nil? || @last_drop_color.nil? || @last_drop_row.nil?
+			return nil
+		end
+		return @last_drop_color if four_in_column || four_in_row ||four_in_upward_diagonal || four_in_downward_diagonal
+		nil
+	end
+
+	private
+
+	def four_in_column
+		return false if @last_drop_row < 3
+
+		count = 0
+		@board[@last_drop_column].each do |cell|
+			break if cell.nil?
+			cell == @last_drop_color ? count += 1 : count = 0
+		end
+		count > 3
+	end
+
+	def four_in_row
+		count = 0
+		@board.each do |column|
+			break if count > 3
+			column[@last_drop_row] == @last_drop_color ? count += 1 : count = 0
+		end
+		count > 3
+	end
+
+	def four_in_upward_diagonal
+		@last_drop_column > @last_drop_row ? min_coordinate = @last_drop_row : min_coordinate = @last_drop_column
+		base_row = @last_drop_row - min_coordinate
+		base_column = @last_drop_column - min_coordinate
+
+
+    count = 0
+		until base_row > 5 || base_column > 6
+			break if count > 3
+			@board[base_column][base_row] == @last_drop_color ? count += 1 : count = 0
+			base_column += 1
+			base_row += 1
+		end
+		count > 3
+	end
+
+	def four_in_downward_diagonal
+		(6-@last_drop_column) > (@last_drop_row) ? min_coordinate = @last_drop_row : min_coordinate = (6-@last_drop_column)
+		base_row = @last_drop_row - min_coordinate
+		base_column = @last_drop_column + min_coordinate
+
+    count = 0
+		until base_row > 5 || base_column < 0
+			break if count > 3
+			@board[base_column][base_row] == @last_drop_color ? count += 1 : count = 0
+			base_column -= 1
+			base_row += 1
+		end
+		count > 3
+	end
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,113 @@
+require_relative '../lib/board.rb'
+
+describe Board do
+
+	let (:board) {Board.new}
+	context "#new" do
+	end
+
+	describe "#drop_disc!" do
+		context "if the column is full" do
+			it "returns true" do
+				board.drop_disc!(1,1).should be_true
+			end
+		end
+
+		context "if the column is full" do
+			it "returns false" do
+				6.times { board.drop_disc!(1, 1) }
+				board.drop_disc!(1,1).should be_false
+			end
+		end 
+	end
+
+	describe "#full?" do
+		context "if the board is not full" do
+			it "returns false" do
+				board.full?.should be_false
+			end
+		end
+	
+		context "if the board is full" do
+			it "returns true" do
+				7.times do |column_index|
+					6.times { board.drop_disc!(column_index + 1, 1) }
+				end
+				board.full?.should be_true
+			end
+		end
+	end
+
+	describe "#color_of_connect_four" do
+		context "if there are no groups of four of the same color" do
+			it "should return nil" do
+				board.color_of_connect_four.should be_nil
+			end
+		end
+
+		context "if there is a group of four of the same color" do
+			it "should return '1'" do
+			end
+		end
+
+		context "if there is a group of four 1's in a column" do
+		  it "should return '1'" do
+		 	  4.times { board.drop_disc!(1, 1) }
+		 	  board.color_of_connect_four.should eq(1)
+		  end
+		end
+
+		context "if there is a group of four of the same color in a row" do
+		  it "should return '1'" do
+		 	  board.drop_disc!(1, 1)
+		 	  board.drop_disc!(2, 1)
+		 	  board.drop_disc!(3, 1)
+		 	  board.drop_disc!(4, 1)
+		 	  board.color_of_connect_four.should eq(1)
+		  end
+		end
+
+		context "if there is a group of four of the same color in an upward sloping diagonal" do
+		  it "should return '2'" do
+		 	  3.times { board.drop_disc!(6, 1) }
+		 	  2.times { board.drop_disc!(6, 2) }
+		 	  board.drop_disc!(5, 1)
+		 	  3.times { board.drop_disc!(5, 2) }
+		 	  3.times { board.drop_disc!(4, 2) }
+		 	  board.drop_disc!(3, 1)
+		 	  board.drop_disc!(3, 2)
+		 	  board.color_of_connect_four.should eq(2)
+		  end
+
+		  	it "should return '1'" do
+		 	  3.times { board.drop_disc!(4, 2) }
+		 	  1.times { board.drop_disc!(4, 1) }
+		 	  3.times { board.drop_disc!(3, 1) }
+		 	  2.times { board.drop_disc!(2, 1) }
+		 	  board.drop_disc!(1, 1)
+		 	  board.color_of_connect_four.should eq(1)
+		  end
+		end
+
+		context "if there is a group of four of the same color in downward sloping diagonal" do
+		  it "should return '1'" do
+		  	3.times { board.drop_disc!(1, 2) }
+		  	board.drop_disc!(1, 1)
+		  	3.times { board.drop_disc!(2, 1) }
+		  	2.times { board.drop_disc!(3, 1) }
+		 	  board.drop_disc!(4, 1)
+		 	  board.color_of_connect_four.should eq(1)
+		  end
+
+		  it "should return '1'" do
+		  	3.times { board.drop_disc!(2, 2) }
+		  	2.times { board.drop_disc!(2, 1) }
+		  	3.times { board.drop_disc!(3, 2) }
+				board.drop_disc!(3, 1)
+				2.times { board.drop_disc!(5, 1) }
+				3.times { board.drop_disc!(4, 1) }
+		 	  board.color_of_connect_four.should eq(1)
+		  end
+		end
+	end
+end
