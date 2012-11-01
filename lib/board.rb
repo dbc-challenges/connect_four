a9d6d00e521e1bab571899e683d8401cace4aca1
Apprentice <apprentice@dbc15.local>
2012-11-01 15:40:16 -0700

    working board with tests

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,6 @@ class Board
 
   def initialize(col_num = 7, row_num = 6)
     @cells = Array.new(col_num * row_num, "")
-        #
     @col_num = col_num
     @row_num = row_num
   end
@@ -15,14 +14,120 @@ class Board
   def place_piece(column, piece)
     position = (col_num * (row_num - 1)) + (column - 1)
     until cells[position].empty?
-      position -= 7
+      position -= col_num
     end
     cells[position] = piece
   end
-  1
 
   def full?
     cells.all? {|cell| cell != "" }
+  end
+
+  def rows
+    cells.each_slice(col_num).collect { |row| row }
+  end
+
+  def columns
+    columns_array = Array.new(col_num) { Array.new([]) }
+    cells.each_with_index do |cell, i|
+      columns_array[i % col_num] << cell
+    end
+    columns_array
+  end
+
+  def get_start_indexes_to_right(win_no)
+    container = []
+    (col_num - (win_no-1)).times do |i|
+      container << i
+    end
+    (row_num - (win_no-1)).times do |i|
+        container << i*col_num unless i == 0
+    end
+    container
+  end
+
+  def get_start_indexes_to_left(win_no)
+    container = []
+    (col_num - win_no+1).times do |i|
+      container << i + (win_no-1)
+    end
+    (row_num - (win_no-1)).times do |i|
+        container << (i*col_num + col_num-1) unless i == 0
+    end
+    container
+  end
+
+  def row_index_number(index)
+    (index / col_num).floor
+  end
+
+  def column_index_number(index)
+    index % col_num
+  end
+
+  def diagonal_indexes_to_right
+     start_indexes = get_start_indexes_to_right(4)
+     leap = col_num + 1
+     start_indexes.map do |index|
+       diagonal = []
+       while true
+         diagonal << index
+         index += leap
+         row, column = row_index_number(index), column_index_number(index)
+         break if row == row_num || column == 0
+       end
+       diagonal
+     end
+   end
+
+  def diagonal_indexes_to_left
+     start_indexes = get_start_indexes_to_left(4)
+     leap = col_num - 1
+     start_indexes.map do |index|
+       diagonal = []
+       while true
+         diagonal << index
+         index += leap
+         row, column = row_index_number(index), column_index_number(index)
+         break if row == row_num || column == col_num-1
+       end
+       diagonal
+     end
+   end
+
+  # def diagonal_indexes(direction)
+  #   start_indexes = get_start_indexes_to_left(4)
+  #   leap = col_num + 1 if direction == "to_right"
+  #   leap = col_num - 1 if direction == "to_left"
+  #   start_indexes.map do |index|
+  #     diagonal = []
+  #     while true
+  #       diagonal << index
+  #       index += leap
+  #       row, column = row_index_number(index), column_index_number(index)
+  #       break if (row == row_num || column == 0) && direction == "to_right"
+  #       break if (row == row_num || column == col_num-1) && direction == "to_left"
+  #     end
+  #     diagonal
+  #   end
+  # end
+
+  def diagonals
+    (diagonal_indexes_to_right + diagonal_indexes_to_left).map {|diagonal| diagonal.map { |i| i = cells[i]}}
+    # (diagonal_indexes("to_right") + diagonal_indexes("to_left")).map {|diagonal| diagonal.map { |i| i = cells[i]}}
+  end
+
+
+  def check_four_consecutive?
+    # (columns + rows + diagonals).any? { |group| group.four_consecutive? }
+    (columns + rows + diagonals).any? { |lines| four_consecutive?(lines) }
+  end
+
+  def four_consecutive?(line)
+    line.each_cons(4) do |element|
+      return true if (element.uniq.size == 1 && element.uniq[0] != "")
+    end
+    false
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -60,6 +60,128 @@ describe Board do
 
   end
 
+   context "#rows" do
+     it "has row_num rows" do # TODO: better way for testing this????
+       @board.rows.length.should eq(@board.row_num)
+     end
+   end
+
+   context "#columns" do
+     it "has col_num columns" do # TODO: better way for testing this????
+       @board.columns.length.should eq(@board.col_num)
+     end
+   end
+
+   context "#get_start_indexes_to_right" do
+     it "should have unique index numbers" do
+       @board.get_start_indexes_to_right(4).uniq.length.should eq(@board.get_start_indexes_to_right(4).length)
+     end
+     it "should only include numbers from the first row and the first column"
+     it "should not include any other numbers than the ones from the first row and the first column"
+     it "should not include the numbers of the first row bigger than 4"
+
+   end
+
+   context "#get_start_indexes_to_left" do
+     it "should have unique index numbers" do
+       @board.get_start_indexes_to_left(4).uniq.length.should eq(@board.get_start_indexes_to_right(4).length)
+     end
+     it "should only include numbers from the first row and the last column"
+     it "should not include any other numbers than the ones from the first row and the last column"
+     it "should not include the numbers smaller than 4"
+   end
+
+   context "#row_index_number" do
+     it "returns the indexnumber of the row of a certain field"
+   end
+
+   context "#column_index_number" do
+     it "returns the indexnumber of the column of a certain field"
+   end
+
+   context "#diagonal_indexes" do
+     it "has a maximum of row_num units when its going to right"
+     it "has a maximum of row_num units when its going to left"
+   end
+
+   context "#diagonals" do
+     it "has diagonals in both directions"
+     it "has gathered the empty contents of the cells" do
+       @board.diagonals[0].should include('')
+     end
+     it "has gathered the contents of the cells" do
+       @board.place_piece(6, "Token")
+       @board.diagonals[0].should include("Token")
+     end
+   end
+
+   context "#check_four_consecutive?" do
+    it "returns true if there are four occupied cells in a row" do
+      @board.place_piece(1, "Striped")
+      @board.place_piece(2, "Striped")
+      @board.place_piece(3, "Striped")
+      @board.place_piece(4, "Striped")
+      @board.check_four_consecutive?.should be_true
+    end
+
+    it "returns false if there are four occupied cells in a row" do
+       @board.place_piece(1, "Striped")
+       @board.place_piece(2, "Striped")
+       @board.place_piece(3, "Striped")
+       @board.place_piece(5, "Striped")
+       @board.check_four_consecutive?.should be_false
+     end
+
+    it "returns true if there are four occupied cells in a column" do
+       @board.place_piece(1, "Striped")
+       @board.place_piece(1, "Striped")
+       @board.place_piece(1, "Striped")
+       @board.place_piece(1, "Striped")
+       @board.check_four_consecutive?.should be_true
+     end
+
+     it "returns false if there are four occupied cells in a row" do
+        @board.place_piece(1, "Striped")
+        @board.place_piece(1, "Striped")
+        @board.place_piece(1, "Striped")
+        @board.place_piece(5, "Striped")
+        @board.check_four_consecutive?.should be_false
+      end
+
+    it "returns true if there are four occupied cells in a column" do
+       @board.place_piece(1, "Test")
+       @board.place_piece(2, "Striped2")
+       @board.place_piece(2, "Test")
+       @board.place_piece(3, "Striped2")
+       @board.place_piece(3, "Striped")
+       @board.place_piece(3, "Test")
+       @board.place_piece(4, "Striped")
+       @board.place_piece(4, "Striped2")
+       @board.place_piece(4, "Striped")
+       @board.place_piece(4, "Test")
+       @board.check_four_consecutive?.should be_true
+     end
+
+     it "returns false if there are four occupied cells in a row" do
+       @board.place_piece(1, "Test_wrong")
+       @board.place_piece(2, "Striped2")
+       @board.place_piece(2, "Test")
+       @board.place_piece(3, "Striped2")
+       @board.place_piece(3, "Striped")
+       @board.place_piece(3, "Test")
+       @board.place_piece(4, "Striped")
+       @board.place_piece(4, "Striped2")
+       @board.place_piece(4, "Striped")
+       @board.place_piece(4, "Test")
+       @board.check_four_consecutive?.should be_false
+     end
+  end
+
+  context "#four_consecutive" do
+    it "returns false, if a line of the board has not four of the same, consecutive tokens in it"
+    it "returns true, if a line of the board has four of the same, consecutive tokens in it"
+  end
+
 
 
 end
