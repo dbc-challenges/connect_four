bfb86526355e503f80c701dd24bda88bd74a25c6
Jonathan Pepin <jypepin@gmail.com>
2012-11-04 21:55:15 -0800

    made the AI play with there is the more consecutive opponent's coins

--- connect4/defense.rb
+++ connect4/defense.rb
@@ -4,7 +4,7 @@ class Defense
   attr_reader :board, :positions_ratings
 
   def initialize
-    #@board = Board.new
+    @board = Board.new
     @positions_ratings = { 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0}
   end
 
@@ -12,7 +12,7 @@ class Defense
     @available_moves = []
     first_row = [35, 36, 37, 38, 39, 40, 41]
     first_row.each do |position|
-      until UI.game.board.cells[position].empty?
+      until board.cells[position].empty?
         position -= board.col_num
       end
       @available_moves << position
@@ -24,65 +24,94 @@ class Defense
     available_moves.each do |move|
       cell_rating_left(move)
       cell_rating_right(move)
+      cell_rating_bottom(move)
     end
     positions_ratings
   end
 
   def cell_rating_left(position)
-    left_cell = UI.game.board.cells[position-1]
+    left_cell = board.cells[position-1]
     unless [35, 28, 21, 14, 7, 0].include?(position)
       if left_cell == "red"
         positions_ratings[(position%7)+1] += 2
-        # cell_rating_left2(position) unless [36, 29, 22, 15, 8, 1].include?(position)
-      else
-        positions_ratings[position%7+1] ||= 0
+        cell_rating_left2(position) unless [36, 29, 22, 15, 8, 1].include?(position)
       end
     end
+    positions_ratings[position%7+1]
   end
 
   def cell_rating_left2(position)
-    left_cell2 = UI.game.board.cells[position-2]
+    left_cell2 = board.cells[position-2]
     if left_cell2 == "red"
       positions_ratings[position%7+1] += 4
-      cell_rating_left_3(position) unless [37, 30, 23, 16, 9, 2].include?(position)
+      cell_rating_left3(position) unless [37, 30, 23, 16, 9, 2].include?(position)
     end
   end
 
-  def cell_rating_left_3(position)
-    left_cell3 = UI.game.board.cells[position-3]
+  def cell_rating_left3(position)
+    left_cell3 = board.cells[position-3]
     if left_cell3 == "red"
       positions_ratings[position%7+1] += 8
     end
   end
 
   def cell_rating_right(position)
-    if position % 7 < 6
-      right_cell = UI.game.board.cells[position+1]
+    right_cell = board.cells[position+1]
+    unless [41, 34, 27, 20, 13, 6].include?(position)
       if right_cell == "red"
-        positions_ratings[position%7+1] = 2
-    else
-      positions_ratings[position%7+1] ||= 0
+        positions_ratings[position%7+1] += 2
+        cell_rating_right2(position) unless [40, 33, 26, 19, 12, 5].include?(position)
       end
+    end
+    positions_ratings[position%7+1]
+  end
+
+  def cell_rating_right2(position)
+    right_cell2 = board.cells[position+2]
+    if right_cell2 == "red"
+      positions_ratings[position%7+1] += 4
+      cell_rating_right3(position) unless [39, 32, 25, 18, 11, 4].include?(position)
+    end
+  end
+
+  def cell_rating_right3(position)
+    right_cell3 = board.cells[position+3]
+    if right_cell3 == "red"
+      positions_ratings[position%7+1] += 8
     end
   end
 
   def cell_rating_bottom(position)
     unless [35, 36, 37, 38, 39, 40, 41].include?(position)
-      bottom_cell = UI.game.board.cells[position+7]
+      bottom_cell = board.cells[position+7]
       if bottom_cell == "red"
-        positions_ratings[position%7+1] = 2
-    else
-      positions_ratings[position%7+1] ||= 0
+        positions_ratings[position%7+1] += 2
+        cell_rating_bottom2(position) unless [28, 29, 30, 31, 32, 33, 34].include?(position)
       end
+    end
+    positions_ratings[position%7+1]
+  end
+
+  def cell_rating_bottom2(position)
+    bottom_cell2 = board.cells[position+14]
+    if bottom_cell2 == "red"
+      positions_ratings[position%7+1] += 4
+      cell_rating_bottom3(position) unless [22, 23, 24, 25, 26, 27].include?(position)
+    end
+  end
+
+  def cell_rating_bottom3(position)
+    bottom_cell3 = board.cells[position+21]
+    if bottom_cell3 == "red"
+      positions_ratings[position%7+1] += 8
     end
   end
 
   def move
     rate_all_cells
     play_column = positions_ratings.max_by {|k, v| v}.first.to_i
-    #board.place_piece(play_column, "black")
-    positions_ratings.clear
-    return play_column
+    board.place_piece(play_column, "black")
+    positions_ratings.each {|k,v| positions_ratings[k] = 0}
   end
 
 end
--- connect4/defense_spec.rb
+++ connect4/defense_spec.rb
@@ -22,22 +22,67 @@ describe Defense do
       deff.cell_rating_left(36).should eq 0
     end
 
-    it "should rate a cell with pieces on the left" do
+    it "should rate 2 a cell with one pieces on the left" do
       deff = Defense.new
       deff.board.place_piece(2, "red")
       deff.cell_rating_left(37).should eq 2
     end
 
-    it "should rate a cell with pieces on the right" do
+    it "should rate 6 a cell with 2 pieces on the left" do
+      deff = Defense.new
+      deff.board.place_piece(2, "red")
+      deff.board.place_piece(1, "red")
+      deff.cell_rating_left(37).should eq 6
+    end
+
+    it "should rate 14 a cell with 2 pieces on the left" do
+      deff = Defense.new
+      deff.board.place_piece(2, "red")
+      deff.board.place_piece(3, "red")
+      deff.board.place_piece(1, "red")
+      deff.cell_rating_left(38).should eq 14
+    end
+
+    it "should rate 2 a cell with one pieces on the right" do
       deff = Defense.new
       deff.board.place_piece(7, "red")
       deff.cell_rating_right(40).should eq 2
     end
 
-    it "should rate a cell with pieces on the bottom" do
+    it "should rate 6 a cell with 2 pieces on the right" do
+      deff = Defense.new
+      deff.board.place_piece(7, "red")
+      deff.board.place_piece(6, "red")
+      deff.cell_rating_right(39).should eq 6
+    end
+
+    it "should rate 14 a cell with 2 pieces on the right" do
+      deff = Defense.new
+      deff.board.place_piece(7, "red")
+      deff.board.place_piece(6, "red")
+      deff.board.place_piece(5, "red")
+      deff.cell_rating_right(38).should eq 14
+    end
+
+    it "should rate 2 a cell with 1 pieces on the bottom" do
       deff = Defense.new
       deff.board.place_piece(4, "red")
       deff.cell_rating_bottom(31).should eq 2
+    end
+
+    it "should rate 6 a cell with 2 pieces on the bottom" do
+      deff = Defense.new
+      deff.board.place_piece(4, "red")
+      deff.board.place_piece(4, "red")
+      deff.cell_rating_bottom(24).should eq 6
+    end
+
+    it "should rate 14 a cell with 3 pieces on the bottom" do
+      deff = Defense.new
+      deff.board.place_piece(4, "red")
+      deff.board.place_piece(4, "red")
+      deff.board.place_piece(4, "red")
+      deff.cell_rating_bottom(17).should eq 14
     end
 
     it "should not rate a friend-neighbor cell" do
@@ -49,7 +94,7 @@ describe Defense do
     it "should rate all the available moves at once" do
       deff = Defense.new
       deff.board.place_piece(7, "red")
-      deff.rate_all_cells.should eq ({1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>2, 7=>0})
+      deff.rate_all_cells.should eq ({1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>2, 7=>2})
     end
   end
 
@@ -57,15 +102,17 @@ describe Defense do
     it "should play the highest rated move" do
       deff = Defense.new
       deff.board.place_piece(7, "red")
-      deff.play
-      deff.board.cells.should eq ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "black", "red"]
+      deff.board.place_piece(7, "red")
+      deff.board.place_piece(7, "red")
+      deff.move
+      deff.board.cells.should eq ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "black", "", "", "", "", "", "", "red", "", "", "", "", "", "", "red", "", "", "", "", "", "", "red"]
     end
 
     it "should clear the grades hash" do
       deff = Defense.new
       deff.board.place_piece(7, "red")
-      deff.play
-      deff.positions_ratings.should be_empty
+      deff.move
+      expect { deff.positions_ratings.all? {|k,v| positions_ratings[k] == 0} }.to be_true
     end
   end
 
